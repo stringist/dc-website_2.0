3ac8a8d6f38aa3a0e1a92f22e4b4fb366c8cd5b1
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-05 15:34:34 +0100

    fix merge conflicts

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,15 @@
-<<<<<<< HEAD
 import "./../styles/globals.scss";
 import Layout from "../components/layout";
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
-=======
-import "../styles/globals.scss";
 import { StoreProvider } from "../utilities/Store";
 import React, { useContext } from "react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <StoreProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
     </StoreProvider>
->>>>>>> about
   );
 }
 
