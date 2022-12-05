9919a9b706b52bb08b158f63e25e34fffdfaa33d
axtecho <89384900+axtecho@users.noreply.github.com>
2022-12-05 21:38:25 +0100

    adding links

---
+++ components/clublinks.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+export default function ClubLinks() {
+  return (
+    <>
+      <Link href="https://www.facebook.com/groups/kfkdiscgolf">
+        KFK Discgolf
+      </Link>
+      <Link href="https://www.facebook.com/groups/4736477413134157">
+        Tune IF Disc Golf
+      </Link>
+      <Link href="https://www.facebook.com/groups/1994859693977329">
+        Ejby IF Disc Golf
+      </Link>
+      <Link href="https://www.facebook.com/groups/storebaelt.discgolf">
+        Eggeslevmagle Discgolf
+      </Link>
+      <Link href="https://www.facebook.com/groups/1024655904253100">
+        Helsingør Disc Golf Venner
+      </Link>
+      <Link href="https://www.ndgk.dk/">Næstved Disc Golf Klub</Link>
+      <Link href="https://www.facebook.com/MDK2630/">Mølleparken Discgolf</Link>
+    </>
+  );
+}
--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,28 @@
 import AboutProfile from "../components/aboutProfile";
+import Head from "next/head";
 import soeren from "../public/images/soeren.png";
-
+import sinus from "../public/images/sinus.png";
+import ClubLinks from "../components/clublinks";
+import Hero from "../components/hero";
 export default function About() {
   return (
     <>
+      <header>
+        <Hero title="About" />
+      </header>
       <AboutProfile
         image={soeren}
         text={
           "Disc Connection er en 2-mands interessentvirksomhed hvis primære formål er at udbrede disc golf-sporten i Danmark via salg af discs og udstyr til disc golf. Vi er startet sammen i foråret 2006 og er den mest alsidige importør og forhandler af golfdiscs og udstyr til disc golf, og har et meget stort og bredt udvalg af kvalitetsprodukter, samt Danmarks bedste priser - uanset om du handler online, i vores Proshop i Tune, eller i vores partnerbutikker i Roskilde, på RUC, i Viby eller på Østerbro."
         }
       />
+      <AboutProfile
+        image={sinus}
+        text={
+          "Vi giver 10% online-rabat (og fysisk i Proshoppen i Tune) på ikke-nedsatte discs og udstyr med en pris på mellem 60 kr. og 2.000 kr. - til medlemmer af KFK Disc Golf, Tune IF Disc Golf, Ejby IF Disc Golf, Eggeslevmagle Discgolf, Helsingør Disc Golf Venner, Næstved Disc Golf Klub og MDK (Mølleparken DiscGolf Klub) , og det kan derfor meget nemt blive en god investering at melde sig ind i en af disse klubber. Læs mere om klubberne på deres respektive Facebook-sider:"
+        }
+      />
+      <ClubLinks></ClubLinks>
     </>
   );
 }
