2de7dbe9ff28a770ea6669e5210f86af10914d16
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-05 15:12:49 +0100

    import global styles to app

--- components/Nav.js
+++ components/Nav.js
@@ -4,39 +4,37 @@ import styles from "../styles/nav.module.scss";
 
 export default function Nav() {
   return (
-    <>
-      <nav>
-        <div className="left-wrapper">
-          {" "}
-          <div className="logo-container"></div>
-          <ul>
-            <li>
-              <Link href="../pages/productList">
-                <a>Discs</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="../pages/productList">
-                <a>Bags</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="../pages/productList">
-                <a>Accesories</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="../pages/about">
-                <a>About</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <nav>
+      <div className="left-wrapper">
+        {" "}
+        <div className="logo-container"></div>
+        <ul>
+          <li>
+            <Link href="../pages/productList">
+              <a>Discs</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="../pages/productList">
+              <a>Bags</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="../pages/productList">
+              <a>Accesories</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="../pages/about">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-        <div className="right-wrapper">
-          <SearchBar />
-        </div>
-      </nav>
-    </>
+      <div className="right-wrapper">
+        <SearchBar />
+      </div>
+    </nav>
   );
 }
