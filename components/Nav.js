49c144d69931c2bc46506aeb12f585b4968d178b
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-05 13:11:23 +0100

    create nav, footer, hero area components

---
+++ components/Nav.js
@@ -0,0 +1,12 @@
+import Link from "next/Link";
+import Styles from "../styles/nav.module.scss";
+
+export default function Nav() {
+  return (
+    <>
+      <nav>
+        <div className="logo-container"></div>
+      </nav>
+    </>
+  );
+}
