70bcd8b52e133f5e02d03d0f4839626b8472c83a
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-05 15:54:32 +0100

    file name capitalization

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "./../styles/globals.scss";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import { StoreProvider } from "../utilities/Store";
 import React, { useContext } from "react";
 
