db2ea84023eaeb674c44e071016c32b87f56257c
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-06 21:23:06 +0100

    footer links

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,24 @@
 import Link from "next/link";
-import styles from '../styles/Footer.module.scss';
+import styles from "../styles/Footer.module.scss";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      div.wrapper>(div.soMe + div.payment + div.schedule)
+      <div className={styles.wrapper}>
+        <div className={styles.soMe}>
+          <a href="https://www.facebook.com/discconnection.dk">
+            <div class={styles.fb} />
+          </a>
+          <a href="https://www.instagram.com/disc_connection_dk/">
+            <div class={styles.ig} />
+          </a>
+          <a href="#">
+            <div class={styles.yt} />
+          </a>
+        </div>
+        <div className={styles.payment}></div>
+        <div className={styles.schedule}></div>
+      </div>
     </footer>
-  )
+  );
 }
