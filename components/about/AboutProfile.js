62d997ad77b1d169af05b95a36b111ba5a0b2041
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-07 12:23:16 +0100

    fix conflicts

--- components/about/AboutProfile.js
+++ components/about/AboutProfile.js
@@ -4,17 +4,10 @@ export default function AboutProfile({ ...props }) {
   console.log(props.name);
   return (
     <div
-      style={
-        props.name === "soeren"
-          ? { flexDirection: "row" }
-          : { flexDirection: "row-reverse" }
-      }
+      style={props.name === "soeren" ? { flexDirection: "row" } : { flexDirection: "row-reverse" }}
       className={styles.profile}
     >
-      <div className= {props.name === "soeren"
-    ? {styles.soeren_img}
-    : {styles.sinus_img}
-    }>
+      <div>
         <Image src={props.image} alt="black haired person" object-fit="fill" />
       </div>
       <p>{props.text}</p>
