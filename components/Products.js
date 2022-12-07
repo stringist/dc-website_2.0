28efcbbbf07586fb64191cda1993983e730c31ef
paulafiaschi <paul6399@stud.kea.dk>
2022-12-07 15:08:29 +0100

    filter almost working

--- components/Products.js
+++ components/Products.js
@@ -1,17 +1,18 @@
 import ProductTile from "./product-tile/ProductTile";
 import { v4 as uuidv4 } from "uuid";
 import React from "react";
-import { useState } from "react";
+import { useState, useConditions } from "react";
 
 export default function Products({ category, products }) {
   const [brandFilter, setBrandFilter] = useState([]);
-  const [filteredByBrand, setFilteredByBrand] = useState(false);
   const [colorFilter, setColorFilter] = useState([]);
   const [speedFilter, setSpeedFilter] = useState([]);
-  const [filteredList, setFilteredList] = useState([]);
-  let results = new Array();
+  const [glideFilter, setGlideFilter] = useState([]);
+  const [turnFilter, setTurnFilter] = useState([]);
+  const [fadeFilter, setFadeFilter] = useState([]);
 
   let categoryList = products.filter((product) => product.category === category);
+  let filteredList = categoryList;
 
   let brands = [...new Set(categoryList.map((product) => product.brand))];
   brands.sort(function (a, b) {
@@ -47,7 +48,13 @@ export default function Products({ category, products }) {
     return a > b;
   });
   let turns = [...new Set(categoryList.map((product) => product.turn))];
+  turns.sort(function (a, b) {
+    return a > b;
+  });
   let fades = [...new Set(categoryList.map((product) => product.fade))];
+  fades.sort(function (a, b) {
+    return a > b;
+  });
 
   function handleBrands(e) {
     e.target.checked && !brandFilter.includes(e.target.value) ? setBrandFilter((old) => old.concat(e.target.value)) : null;
@@ -64,69 +71,123 @@ export default function Products({ category, products }) {
     let speedNumber = parseFloat(e.target.value);
     e.target.checked && !speedFilter.includes(speedNumber) ? setSpeedFilter((old) => old.concat(speedNumber)) : null;
     !e.target.checked && speedFilter.includes(speedNumber) ? setSpeedFilter((old) => old.filter((speed) => speed !== speedNumber)) : null;
+    filterList();
+  }
+  function handleGlide(e) {
+    let glideNumber = parseFloat(e.target.value);
+    e.target.checked && !speedFilter.includes(glideNumber) ? setSpeedFilter((old) => old.concat(glideNumber)) : null;
+    !e.target.checked && speedFilter.includes(glideNumber) ? setSpeedFilter((old) => old.filter((glide) => glide !== glideNumber)) : null;
+    filterList();
+  }
+  function handleTurn(e) {
+    let turnNumber = parseFloat(e.target.value);
+    e.target.checked && !speedFilter.includes(turnNumber) ? setSpeedFilter((old) => old.concat(turnNumber)) : null;
+    !e.target.checked && speedFilter.includes(turnNumber) ? setSpeedFilter((old) => old.filter((turn) => turn !== turnNumber)) : null;
+    filterList();
+  }
+  function handleFade(e) {
+    let fadeNumber = parseFloat(e.target.value);
+    e.target.checked && !speedFilter.includes(fadeNumber) ? setSpeedFilter((old) => old.concat(fadeNumber)) : null;
+    !e.target.checked && speedFilter.includes(fadeNumber) ? setSpeedFilter((old) => old.filter((fade) => fade !== fadeNumber)) : null;
+    filterList();
   }
 
-  // function filterList() {
-  //   let filteredList = categoryList;
-  //   if (brandFilter.length > 0) {
-  //     // results = brandFilter.map((brand) => categoryList.filter((product) => product.brand === brand));
-
-  //     var keys = ["brand"];
-  //     var values = brandFilter;
-
-  //     var result = categoryList.filter(function (e) {
-  //       return keys.every(function (a) {
-  //         return values.includes(e[a]);
-  //       });
-  //     });
-  //     filteredList = result;
-  //     // console.log("step 1 results", result);
-  //   }
-
-  //   console.log("filteredLisssssst", filteredList);
-
-  //   if (colorFilter.length > 0) {
-  //     // // results = colorFilter.map((color) => results[0].filter((product) => product.color === color));
-  //     // var keys = ["color"];
-  //     // var values = colorFilter;
-  //     // var result = results.filter(function (e) {
-  //     //   return keys.every(function (a) {
-  //     //     return values.includes(e[a]);
-  //     //   });
-  //     // });
-  //     // // results = result;
-  //     // console.log("step 2 results", result);
-  //   }
-  //   if (speedFilter.length > 0) {
-  //     // results = colorFilter.map((color) => results[0].filter((product) => product.color === color));
-  //     var keys = ["speed"];
-  //     var values = speedFilter;
-
-  //     var result = categoryList.filter(function (e) {
-  //       return keys.every(function (a) {
-  //         return values.includes(e[a]);
-  //       });
-  //     });
-
-  //     // results = result;
-  //     console.log("step 2 results", filteredList);
-  //   }
-  // }
-
   function filterList() {
-    var useConditions = (search) => (a) =>
-        Object.keys(search).every(
-          (k) => a[k] === search[k] || (Array.isArray(search[k]) && search[k].includes(a[k])) || (typeof search[k] === "object" && +search[k].min <= a[k] && a[k] <= +search[k].max) || (typeof search[k] === "function" && search[k](a[k]))
-        ),
-      data = [
-        { id: "123", color: "Red", model: "Tesla" },
-        { id: "124", color: "Black", model: "Honda" },
-        { id: "125", color: "Red", model: "Audi" },
-        { id: "126", color: "Blue", model: "Tesla" },
-      ],
-      filters = { color: ["Red", "Blue"], model: "Tesla" };
+    filterByBrand();
 
-    console.log(data.filter(useConditions(filters)));
+    function filterByBrand() {
+      if (brandFilter.length > 0) {
+        var keys = ["brand"];
+        var values = brandFilter;
+
+        var result = categoryList.filter(function (product) {
+          return keys.every(function (filter) {
+            return values.includes(product[filter]);
+          });
+        });
+        filteredList = result;
+      }
+      console.log("step 1:", filteredList);
+
+      filterByColor(filteredList);
+
+      function filterByColor(filteredList) {
+        if (colorFilter.length > 0) {
+          var keys = ["color"];
+          var values = speedFilter;
+
+          var result = filteredList.filter(function (product) {
+            return keys.every(function (filter) {
+              return values.includes(product[filter]);
+            });
+          });
+          return (filteredList = result);
+        }
+        console.log("step 2:", filteredList);
+        filterBySpeed(filteredList);
+      }
+
+      function filterBySpeed(filteredList) {
+        if (speedFilter.length > 0) {
+          var keys = ["speed"];
+          var values = speedFilter;
+
+          var result = filteredList.filter(function (product) {
+            return keys.every(function (filter) {
+              return values.includes(product[filter]);
+            });
+          });
+          return (filteredList = result);
+        }
+        console.log("step 3:", filteredList);
+        filterByGlide(filteredList);
+      }
+
+      function filterByGlide(filteredList) {
+        if (glideFilter.length > 0) {
+          var keys = ["glide"];
+          var values = glideFilter;
+
+          var result = filteredList.filter(function (product) {
+            return keys.every(function (filter) {
+              return values.includes(product[filter]);
+            });
+          });
+          filteredList = result;
+        }
+        console.log("step 4:", filteredList);
+        filterByTurn(filteredList);
+      }
+      function filterByTurn(filteredList) {
+        if (turnFilter.length > 0) {
+          var keys = ["turn"];
+          var values = turnFilter;
+
+          var result = filteredList.filter(function (product) {
+            return keys.every(function (filter) {
+              return values.includes(product[filter]);
+            });
+          });
+          filteredList = result;
+        }
+        console.log("step 5:", filteredList);
+        filterByFade(filteredList);
+      }
+      function filterByFade(filteredList) {
+        if (fadeFilter.length > 0) {
+          var keys = ["fade"];
+          var values = fadeFilter;
+
+          var result = filteredList.filter(function (product) {
+            return keys.every(function (filter) {
+              return values.includes(product[filter]);
+            });
+          });
+          filteredList = result;
+        }
+        // console.log("step 6:", filteredList);
+      }
+    }
   }
 
   return (
@@ -134,7 +195,6 @@ export default function Products({ category, products }) {
       <div className="category-page">
         <div className="filters">
           <p>Brand</p>
-          {console.log(speedFilter)}
           {brands.map((brand) => (
             <>
               <input type="checkbox" id={brand} name={brand} value={brand} onChange={handleBrands}></input>
@@ -157,8 +217,26 @@ export default function Products({ category, products }) {
             </>
           ))}
           <p>Glide</p>
+          {glides.map((speed) => (
+            <>
+              <input type="checkbox" id={speed} name={speed} value={speed} onChange={handleSpeed}></input>
+              <label htmlFor={speed}>{speed}</label>
+            </>
+          ))}
           <p>Turn</p>
+          {turns.map((speed) => (
+            <>
+              <input type="checkbox" id={speed} name={speed} value={speed} onChange={handleSpeed}></input>
+              <label htmlFor={speed}>{speed}</label>
+            </>
+          ))}
           <p>Fade</p>
+          {fades.map((speed) => (
+            <>
+              <input type="checkbox" id={speed} name={speed} value={speed} onChange={handleSpeed}></input>
+              <label htmlFor={speed}>{speed}</label>
+            </>
+          ))}
           <p>Price range</p>
         </div>
         <div className="products">
@@ -166,8 +244,8 @@ export default function Products({ category, products }) {
           <div className="product-grid">
             {filterList(brandFilter, colorFilter, speedFilter)}
 
-            {console.log("brandFilter", brandFilter.length > 0)}
-            {categoryList.map((product) => (
+            {/* {console.log("brandFilter", brandFilter.length > 0)} */}
+            {filteredList.map((product) => (
               <ProductTile product={product} key={product._id} />
             ))}
           </div>
