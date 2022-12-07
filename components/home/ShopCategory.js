49968642d407152f3ef45a84d77bfe9ab3ed633e
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-07 21:04:27 +0100

    category section styling

--- components/home/ShopCategory.js
+++ components/home/ShopCategory.js
@@ -1,25 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "../../styles/Home.module.scss";
 export default function ShopCategory() {
   return (
     <section id="category">
       <h2>Shop By Category</h2>
-      <div className="category-group">
-        <div className="category-card">
-          <Image src="/../public/images/placeholder_disc.png" alt="Discs" width="100%" height="300px"></Image>
-          <Link href="/productList">
+      <div className={styles.categoryGroup}>
+        <div className={styles.categoryCard}>
+          <Image src="/../public/images/placeholder_disc.png" alt="Discs" width={200} height={200}></Image>
+          <Link href="/productList/Discs">
             <a>Discs</a>
           </Link>
         </div>
-        <div className="category-card">
-          <Image src="/../public/images/backpack.png" alt="Bags" width="100%" height="300px"></Image>
-          <Link href="/productList">
+        <div className={styles.categoryCard}>
+          <Image src="/../public/images/backpack.png" alt="Bags" width={200} height={200}></Image>
+          <Link href="/productList/Bags">
             <a>Bags</a>
           </Link>
         </div>
-        <div className="category-card">
-          <Image src="/../public/images/placeholder_hat.png" alt="Accessories" width="100%" height="300px"></Image>
-          <Link href="/productList">
+        <div className={styles.categoryCard}>
+          <Image src="/../public/images/placeholder_hat.png" alt="Accessories" width={200} height={200}></Image>
+          <Link href="/productList/Accessories">
             <a>Accessories</a>
           </Link>
         </div>
