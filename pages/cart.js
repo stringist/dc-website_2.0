27ae04c040cd1ba12aa487126071d6ff78a0e347
paulafiaschi <paul6399@stud.kea.dk>
2022-12-08 22:03:19 +0100

    cart table and summary

--- pages/cart.js
+++ pages/cart.js
@@ -2,6 +2,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Image from "next/image";
 import { incrementQuantity, decrementQuantity, removeFromCart } from "./redux/cart.slice";
 
+import styles from "../styles/Cart.module.scss";
+
 export default function Cart() {
   // Extracting cart state from redux store
   const cart = useSelector((state) => state.cart);
@@ -15,38 +17,58 @@ export default function Cart() {
   }
 
   return (
-    <div>
-      {cart.length === 0 ? (
-        <h1>Your Cart is Empty!</h1>
-      ) : (
-        <>
-          <div>
-            <div>Image</div>
-            <div>item</div>
-            <div>Price</div>
-            <div>Quantity</div>
-            <div>Actions</div>
-            <div>Total Price</div>
-          </div>
-          {cart.map((item) => (
-            <div key={item._id}>
-              <div>
-                <Image src={item.img} height="90" width="65" alt={item.name} />
-              </div>
-              <p>{item.item}</p>
-              <p>$ {item.price}</p>
-              <p>{item.quantity}</p>
-              <div>
-                <button onClick={() => dispatch(incrementQuantity(item._id))}>+</button>
-                <button onClick={() => dispatch(decrementQuantity(item._id))}>-</button>
-                <button onClick={() => dispatch(removeFromCart(item._id))}>x</button>
-              </div>
-              <p>$ {item.quantity * item.price}</p>
-            </div>
-          ))}
-          <h2>Grand Total: {getTotalPrice()} dkk</h2>
-        </>
-      )}
-    </div>
+    <>
+      <h1>Cart</h1>
+      <div className={styles.cart_section}>
+        {cart.length === 0 ? (
+          <div className={styles.empty_cart}>Your Cart is Empty!</div>
+        ) : (
+          <>
+            <table>
+              <tr>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price per item</th>
+                <th>Subtotal</th>
+                <th></th>
+              </tr>
+              {cart.map((item) => (
+                <tr className={styles.cart_item} key={item._id}>
+                  <td className={styles.cart_image}>
+                    <Image src={item.img} height="90" width="90" alt={item.name} responsive />
+
+                    <p>{item.name}</p>
+                  </td>
+                  <td>{item.price}</td>
+
+                  <td>
+                    <div className={styles.cart_quantity}>
+                      <button onClick={() => dispatch(decrementQuantity(item._id))}>-</button> <span>{item.quantity}</span> <button onClick={() => dispatch(incrementQuantity(item._id))}>+</button>
+                    </div>
+                  </td>
+                  <td>{item.quantity * item.price} dkk</td>
+                  <td className={styles.cart_remove}>
+                    <button onClick={() => dispatch(removeFromCart(item._id))}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                        <path
+                          fill-rule="evenodd"
+                          d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                        />
+                      </svg>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </table>
+            <aside className={styles.cart_summary}>
+              <h2>Summary</h2>
+              <p>Subtotal: {getTotalPrice()} dkk</p>
+              <p>Delivery: 0 dkk</p>
+            </aside>
+          </>
+        )}
+      </div>
+    </>
   );
 }
