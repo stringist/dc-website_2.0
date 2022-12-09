e79f37b3efd5a71b51333eec00e329d9d8f5ca08
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-09 16:05:52 +0100

    added brand list and logo imgs

---
+++ components/home/BrandNames.js
@@ -0,0 +1,34 @@
+// import Image from "next/image";
+
+const BrandNames = [
+  "Axiom",
+  "DGA",
+  "Discmania",
+  "Discraft",
+  "Discstroyer",
+  "Dynamic Discs",
+  "Gateway",
+  "Innova",
+  "Kastaplast",
+  "Latitude 64",
+  "Legacy",
+  "Løft",
+  "Millennium",
+  "MVP",
+  "Northstar",
+  "Prodigy",
+  "Prodiscus",
+  "Streamline",
+  "Westside",
+];
+
+export default BrandNames;
+// const BrandsList = brandNames.map((brand, i) => (
+//   <div className="brandLogo" key={i}>
+//     <Image src={`../../public/images/brands/${brand}.png`} alt={brand} width={200} height={200} />
+//   </div>
+// ));
+
+// console.log(BrandsList);
+
+// export default BrandsList;
--- components/home/BrandsGallery.js
+++ components/home/BrandsGallery.js
@@ -1,10 +1,13 @@
 import styles from "../../styles/Home.module.scss";
+import BrandNames from "./BrandNames";
 
 export default function BrandsGallery() {
+  let BrandsList = [];
+
   return (
     <section className={styles.brands}>
       <h2>Brands</h2>
-      <div className={styles.logoList}></div>
+      {/* <BrandsList /> */}
     </section>
   );
 }
