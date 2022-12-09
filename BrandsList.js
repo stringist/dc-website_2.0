66cfe5eeebffaa4e223b803f8b7735fdaefcf4d4
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-09 15:12:08 +0100

    whatever i jsut did

---
+++ BrandsList.js
@@ -0,0 +1,24 @@
+const brandNames = [
+  Axiom,
+  DGA,
+  Discmania,
+  Discraft,
+  Discstroyer,
+  Dynamic,
+  Gateway,
+  Innova,
+  Kastaplast,
+  Latitude,
+  Legacy,
+  Løft,
+  Millennium,
+  MVP,
+  Northstar,
+  Prodigy,
+  Prodiscus,
+  Streamline,
+  Trilogy,
+  Westside,
+];
+
+let brandList = [];
