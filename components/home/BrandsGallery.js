95f6b2f81f6ad799d67cb49dc8adc32c67873be8
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-09 21:07:26 +0100

    logos mapped and working in Brandsgallery

--- components/home/BrandsGallery.js
+++ components/home/BrandsGallery.js
@@ -1,13 +1,18 @@
 import styles from "../../styles/Home.module.scss";
 import BrandNames from "./BrandNames";
+import Image from "next/image";
+import { v4 as uuidv4 } from "uuid";
+// import logos from "../../public/images/brands";
 
 export default function BrandsGallery() {
-  let BrandsList = [];
-
+  const names = BrandNames.map((name) => name.toLowerCase());
+  const path = "/../public/images/brands/1x/";
   return (
     <section className={styles.brands}>
       <h2>Brands</h2>
-      {/* <BrandsList /> */}
+      {names.map((brand) => (
+        <Image src={`${path}${brand}.png`} alt={brand} width={200} height={200} key={uuidv4} />
+      ))}
     </section>
   );
 }
