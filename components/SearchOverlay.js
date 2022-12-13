9c14db590cbdf9e6805cf95fb419d995e39238c7
paulafiaschi <paul6399@stud.kea.dk>
2022-12-13 14:38:07 -0300

    headers setup

--- components/SearchOverlay.js
+++ components/SearchOverlay.js
@@ -1,13 +1,24 @@
 import Styles from "../styles/Search.module.scss";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 export default function SearchOverlay(props) {
   const [searched, setSearched] = useState("");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const options = { method: "GET", url: "https://cocktails-240e.restdb.io/rest/disc-connection", headers: { "cache-control": "no-cache", "x-apikey": "a7a3d1237d76a4c6bd5943e4230d2b86f526e" } };
+
+    fetch(options)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts({ data: data });
+      });
+  }, []);
 
   const searchGroup = useRef();
 
   console.log("overlay opened");
-  console.log(props);
+  console.log(products);
 
   function toggleOverlay() {
     props.overlayIsOpened ? props.setIsOpening(true) : props.setIsClosing(true);
--- components/layout.js
+++ components/layout.js
@@ -12,19 +12,3 @@ export default function Layout({ children }, { products }) {
     </>
   );
 }
-
-export async function getStaticProps() {
-  const req = await fetch("https://cocktails-240e.restdb.io/rest/disc-connection", {
-    method: "GET",
-    headers: {
-      "cache-control": "no-cache",
-      "x-apikey": "a7a3d1237d76a4c6bd5943e4230d2b86f526e",
-    },
-  });
-  const data = await req.json();
-
-  if (data)
-    return {
-      props: { products: data },
-    };
-}
