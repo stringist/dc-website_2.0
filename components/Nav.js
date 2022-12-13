38e3d86345178b1ceb2544748e896c771650b1aa
Kalle <kalletiihonen@hotmail.com>
2022-12-13 11:24:45 +0100

    router classnames in nav work, not finished

--- components/Nav.js
+++ components/Nav.js
@@ -2,18 +2,21 @@ import Link from "next/link";
 import SearchBar from "./SearchBar";
 import { useSelector } from "react-redux";
 import styles from "../styles/Nav.module.scss";
+// import { ActiveLink } from 'next-active-link';
 import { useRouter } from "next/router";
 
+
 export default function Nav() {
+  // use router for active link styling
   const router = useRouter();
   // Selecting cart from global state
   const cart = useSelector((state) => state.cart);
-
+  
   // Getting the count of items
   const getItemsCount = () => {
     return cart.reduce((accumulator, item) => accumulator + item.quantity, 0);
   };
-
+  
   return (
     <nav className={styles.nav}>
       <div className={styles.leftWrapper}>
@@ -21,12 +24,16 @@ export default function Nav() {
           <div className={styles.logoContainer}></div>
         </Link>
         <ul>
-          <li className={router.pathname == "/productList/Discs" ? styles.active : ""}>
-            <Link href="/productList/Discs">
+          <li><button type="button" onClick={() => console.log(router.query.category)}>Check route</button></li>
+          <li className={router.query.category == "Discs" ? styles.active : ""}>
+            <Link href="/productList/Discs"><a onClick={() => router.push('/productList/Discs')}>Discs</a></Link>
+            {/* <ActiveLink href="/productList/Discs" activeClassName={styles.active}  onActiveChange={(isActive) => console.log(isActive)} activeMatchOptions={{
+        exact: true
+      }}>
               <a>Discs</a>
-            </Link>
+            </ActiveLink> */}
           </li>
-          <li>
+          <li className={router.pathname == "/productList/[category]" ? styles.active : "notactive"}>
             <Link href="/productList/Bags">
               <a>Bags</a>
             </Link>
