19d87d584efa11346bf93b07a10c5e821ca08eaa
paulafiaschi <paul6399@stud.kea.dk>
2022-12-15 11:23:05 -0300

    error pages

--- checkout.js
+++ checkout.js
@@ -16,7 +16,9 @@ export async function checkout({ items }) {
   await stripe.redirectToCheckout({
     mode: "payment",
     lineItems,
-    successUrl: `${window.location.origin}?session_id={CHECKOUT_SESSION_ID}`,
-    cancelUrl: window.location.origin,
+    successUrl: "/thank-you",
+    cancelUrl: "/error",
+    // successUrl: `${window.location.origin}?session_id={CHECKOUT_SESSION_ID}`,
+    // cancelUrl: window.location.origin,
   });
 }
---
+++ pages/error.js
@@ -0,0 +1,31 @@
+import AboutProfile from "../components/about/AboutProfile";
+import Head from "next/head";
+import Link from "next/link";
+
+import Hero from "../components/Hero";
+import { BiErrorAlt } from "react-icons/bi";
+
+import styles from "../styles/Stripe.module.scss";
+export default function Error() {
+  return (
+    <>
+      <Head>
+        <title>Disc Connection</title>
+        <meta name="description" content="About Disc Connection" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>{/* <Hero title="About" /> */}</header>
+
+      <section className={styles.section}>
+        <BiErrorAlt />
+
+        <h2>Oops, something went wrong!</h2>
+
+        <p>
+          Go back to the <Link href="/">home page</Link>
+        </p>
+      </section>
+    </>
+  );
+}
