10bef2a069b39b05f23ab40eadeb8ba6bccf7857
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-12-15 09:58:36 +0100

    move brand imgs

---
+++ components/searchBar 2.js
@@ -0,0 +1,34 @@
+import { React, useState } from "react";
+import SearchOverlay from "./SearchOverlay";
+import Styles from "../styles/Search.module.scss";
+
+export default function SearchBar(props) {
+  const [overlayIsOpened, setOverlayIsOpened] = useState(false);
+  const [isOpening, setIsOpening] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  function toggleOverlay() {
+    overlayIsOpened ? setIsOpening(true) : setIsClosing(true);
+    setOverlayIsOpened((prev) => !prev);
+  }
+
+  return (
+    // this is placeholder
+    <>
+      {overlayIsOpened ? <></> : null}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        // class="bi bi-search"
+        viewBox="0 0 16 16"
+        onClick={toggleOverlay}
+      >
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+      </svg>
+
+      {overlayIsOpened ? <SearchOverlay setOverlayIsOpened={setOverlayIsOpened} isOpening={isOpening} setIsOpening={setIsOpening} isClosing={isClosing} setIsClosing={setIsClosing} products={props.products} /> : null}
+    </>
+  );
+}
