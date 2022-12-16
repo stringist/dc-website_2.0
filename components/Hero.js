d78a7a8fe6b2a5eb45b21d5998b3b4e7069dadce
paulafiaschi <paul6399@stud.kea.dk>
2022-12-15 23:25:57 -0300

    hero images paths

--- components/Hero.js
+++ components/Hero.js
@@ -1,20 +1,13 @@
 import Image from "next/image";
 // import bg from "https://lumensity.dk/dc/basket_hero_full.png";
-import homeBG from "./../public/images/hero_vintage.png";
+import homeBG from "../public/images/hero_vintage.png";
 // import homeBG from "./../public/images/hero_2.png";
-import aboutBG from "./../public/images/basket_hero_full.png";
+import aboutBG from "../public/images/basket_hero_full.webp";
 
 function Hero({ title }) {
   return (
     <>
-      <div
-        className="heroWrapper"
-        style={
-          title === "About"
-            ? { backgroundImage: `url(${aboutBG.src})`, backgroundPositionY: "bottom 30%" }
-            : { backgroundImage: `url(${homeBG.src})`, backgroundPositionY: "top" }
-        }
-      >
+      <div className="heroWrapper" style={title === "About" ? { backgroundImage: `url(${aboutBG.src})`, backgroundPositionY: "bottom 30%" } : { backgroundImage: `url(${homeBG.src})`, backgroundPositionY: "top" }}>
         <h1>{title}</h1>
       </div>
       <div className="scheduleCard">
