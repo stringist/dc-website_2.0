9f07f64c4f5d493e57521f44bd9d207339ddfcf7
axtecho <89384900+axtecho@users.noreply.github.com>
2023-01-09 08:08:32 +0100

    About_confusion

--- components/about/AboutProfile.js
+++ components/about/AboutProfile.js
@@ -2,21 +2,18 @@ import Image from "next/image";
 import styles from "../../styles/About.module.scss";
 export default function AboutProfile({ ...props }) {
   return (
-    <section className={props.name === "Søren" ? styles.profile : styles.profile_reverse}>
+    <section
+      className={
+        props.name === "Søren" ? styles.profile : styles.profile_reverse
+      }
+    >
       <div>
         <Image src={props.image} alt="/" object-fit="fill" />
         <p>{props.name}</p>
       </div>
-      <p className={props.name === "Søren" ? styles.soeren_p : styles.sinus_p}>{props.text}</p>
+      <p className={props.name === "Søren" ? styles.soeren_p : styles.sinus_p}>
+        {props.text}
+      </p>
     </section>
   );
-}
-{
-  /* <ul className={`
-${menuStyles.menuNav} 
-${menuOpen ? menuStyles.showMenu : null} 
-${isOpening ? menuStyles.showingMenu : null} 
-${isClosing ? menuStyles.closingMenu : null}`}>
-
-</ul> */
 }
--- pages/about.js
+++ pages/about.js
@@ -7,6 +7,7 @@ import FindUs from "../components/about/Findus";
 import Hero from "../components/Hero";
 import GetInTouch from "../components/about/GetInTouch";
 import styles from "../styles/About.module.scss";
+import Link from "next/link";
 export default function About() {
   return (
     <>
@@ -24,16 +25,14 @@ export default function About() {
         <section>
           <AboutProfile
             image={soeren}
-            text={
-              `Disc Connection is a 2-man stakeholder company whose primary purpose
+            text={`Disc Connection is a 2-man stakeholder company whose primary purpose
               is to spread the sport of disc golf in Denmark via sale of discs and
               equipment for disc golf. We started together in the spring of 2006,
               and are the most versatile importer and dealer of golf discs and
               equipment for disc golf, and have a very large inventory and wide
               selection of quality products, as well as Denmark's best prices -
               regardless of whether you shop online, in our Proshop in Tune, or in
-              our partner stores in Roskilde, at RUC, in Viby, or at Østerbro.`
-            }
+              our partner stores in Roskilde, at RUC, in Viby, or at Østerbro.`}
             name={"Søren"}
             className={styles.profile}
           />
@@ -41,9 +40,7 @@ export default function About() {
           <AboutProfile
             image={sinus}
             name={"Sinus"}
-            text={
-              `We give a 10% online discount (and physically in the Proshop in Tune); on non-discounted discs and equipment with a price between DKK 60 and DKK 2,000 - to members of KFK Disc Golf, Tune IF Disc Golf, Ejby IF Disc Golf, Eggeslevmagle Discgolf, Helsingør Disc Golf Friends, Næstved Disc Golf Klub and MDK (Mølleparken DiscGolf Club), and it can therefore very easily become a good investment to join one of these clubs. Read more about the clubs on their respective Facebook pages:`
-            }
+            text={`We give a 10% online discount (and physically in the Proshop in Tune); on non-discounted discs and equipment with a price between DKK 60 and DKK 2,000 - to members of KFK Disc Golf, Tune IF Disc Golf, Ejby IF Disc Golf, Eggeslevmagle Discgolf, Helsingør Disc Golf Friends, Næstved Disc Golf Klub and MDK (Mølleparken DiscGolf Club), and it can therefore very easily become a good investment to join one of these clubs. Read more about the clubs on their respective Facebook pages:`}
           />
           <ClubLinks />
         </section>
