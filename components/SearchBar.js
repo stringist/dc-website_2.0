5555aff6f27871a04a19545c65d104d73417a9d2
paulafiaschi <paul6399@stud.kea.dk>
2023-01-10 09:20:58 +0100

    onClick removed

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,5 +1,4 @@
 import { React, useState } from "react";
-import SearchOverlay from "./SearchOverlay";
 import Styles from "../styles/Search.module.scss";
 import Link from "next/link";
 
@@ -15,7 +14,6 @@ export default function SearchBar() {
             fill="currentColor"
             // class="bi bi-search"
             viewBox="0 0 16 16"
-            onClick={toggleOverlay}
             id={Styles.icon}
           >
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
